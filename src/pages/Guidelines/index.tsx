eb69fc54c5b82d8dc6786a9dcbf20b33d3c3ec9e
wirednkod <wirednkod@gmail.com>
2024-01-13 19:09:40 +0200

    Fix front Page and add Guidelines page

---
+++ src/pages/Guidelines/index.tsx
@@ -0,0 +1,16 @@
+// Copyright 2024 @polkadot-blockchain-academy/alumni-hub authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import { PageRow, PageTitle } from "@polkadot-cloud/react"
+import MdxCode from "./index.mdx"
+
+export const Guidelines = () => {
+  return (
+    <>
+      <PageTitle title="The Polkadot Blockchain Academy Community Guidelines" />
+      <PageRow style={{ paddingTop: "1rem" }}>
+        <MdxCode />
+      </PageRow>
+    </>
+  )
+}
